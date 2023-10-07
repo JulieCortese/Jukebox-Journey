5b518a93aaea5e4b1567fe3cb406271b7771e5a7
julie <jcortese@ufl.edu>
2023-10-07 02:17:08 -0400

    used some old code to make a start screen real quick. isn't running yet bc of some pygame troubles i think but it should work soon

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
 # Hi this is the first commit to this repo
 import pygame
 import sys
+from constants import *
+
+
+def draw_game_start(screen):
+    # Initialize title font
+    start_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
+    # Color background
+    screen.fill(BG_COLOR)
+
+    # Initialize and draw title
+    title_surface = start_title_font.render("Jukebox Journey", 0, LINE_COLOR)
+    title_rectangle = title_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    # Initialize buttons
+    # Initialize text first
+    start_text = button_font.render("Start", 0, (255, 255, 255))
+    quit_text = button_font.render("Quit", 0, (255, 255, 255))
+
+    # Initialize button background color and text
+    start_surface = pygame.Surface((start_text.get_size()[0] + 20, start_text.get_size()[1] + 20))
+    start_surface.fill(LINE_COLOR)
+    start_surface.blit(start_text, (10, 10))
+
+    quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
+    quit_surface.fill(LINE_COLOR)
+    quit_surface.blit(quit_text, (10, 10))
+
+    # Initialize button rectangle
+    start_rectangle = start_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 25))
+    quit_rectangle = quit_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 100))
+
+    # Draw buttons
+    screen.blit(start_surface, start_rectangle)
+    screen.blit(quit_surface, quit_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rectangle.collidepoint(event.pos):
+                    mode = 1  # start game
+                    return mode
+                elif quit_rectangle.collidepoint(event.pos):
+                    mode = 2  # close program
+                    return mode
+        pygame.display.update()
 
 
 def main():
     pygame.init()
+    while True:
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Sudoku")
+
+        begin = draw_game_start(screen)
+
+        screen.fill(BG_COLOR)
+
+        if begin == 1:
+            # start game.
+            print("user clicked start game")
+        elif begin == 2:
+            #do something else
+            print("user clicked quit game")
+            sys.exit()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
